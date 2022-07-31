1e7b573e929c6e6b394cf256f757366c62a1deae
phuchataydev <phucgithub@gmail.com>
2022-07-31 20:53:29 +0700

    test

---
+++ src/components/Layout/index.js
@@ -0,0 +1,2 @@
+export { default as DefaultLayout } from './DefaultLayout';
+export { default as HeadrOnly } from './HeaderOnly';
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,11 @@
+import Header from '~/components/Layout/components/Header';
+
+function Upload() {
+    return (
+        <div>
+            <h2>upload</h2>
+        </div>
+    );
+}
+
+export default Upload;
